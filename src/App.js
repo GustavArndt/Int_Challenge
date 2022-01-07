a36335eec386108eec7cb2920fc3f2a2e250bca1
GustavArndt <51105784+GustavArndt@users.noreply.github.com>
2022-01-07 11:04:10 -0300

    front-end updated
    front-end updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import InputInterface from "./components/input/InputInterface";
+import ChartInterface from "./components/chart/ChartInterface";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="data-screen">
+        <InputInterface />
+        <ChartInterface />
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/chart/ChartInterface.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ChartInterface() {
+    return (
+        <div className='chart-interface'>
+            ChartInterface
+        </div>
+    )
+}
